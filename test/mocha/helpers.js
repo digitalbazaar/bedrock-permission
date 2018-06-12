08ac98617052b68b1f2cffbde736a47878a60aee
Dave Longley <dlongley@digitalbazaar.com>
2018-06-12 10:37:23 -0400

    Use new bedrock-identity API in tests.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -4,7 +4,6 @@
 /* jshint node: true */
 'use strict';
 
-// FIXME: update to use new bedrock-identity API
 const brIdentity = require('bedrock-identity');
 const database = require('bedrock-mongodb');
 const {promisify} = require('util');
@@ -16,25 +15,18 @@ module.exports = api;
 api.createIdentity = userName => {
   const newIdentity = {
     id: 'did:' + uuid(),
-    type: 'Identity',
-    sysSlug: userName,
     label: userName,
     email: userName + '@bedrock.dev',
-    sysPassword: 'password',
-    sysPublic: ['label', 'url', 'description'],
-    sysResourceRole: [],
     url: 'https://example.com',
-    description: userName,
-    sysStatus: 'active'
+    description: userName
   };
   return newIdentity;
 };
 
 api.getActors = async mockData => {
   const actors = {};
-  const getFn = promisify(brIdentity.get);
   for(const [key, record] of Object.entries(mockData.identities)) {
-    actors[key] = await getFn(null, record.identity.id);
+    actors[key] = await brIdentity.getCapabilities({id: record.identity.id});
   }
   return actors;
 };
@@ -56,15 +48,12 @@ api.prepareDatabase = async mockData => {
 
 async function insertTestData(mockData) {
   const records = Object.values(mockData.identities);
-  // FIXME: use new bedrock-identity API
-  const insertFn = promisify(brIdentity.insert);
   for(const record of records) {
     try {
-      await insertFn(null, record.identity);
+      await brIdentity.insert(
+        {actor: null, identity: record.identity, meta: record.meta || {}});
     } catch(e) {
-      // FIXME: use new bedrock-identity API
-      //if(e.name === 'DuplicateError') {
-      if(!database.isDuplicateError(e)) {
+      if(e.name === 'DuplicateError') {
         // duplicate error means test data is already loaded
         continue;
       }
